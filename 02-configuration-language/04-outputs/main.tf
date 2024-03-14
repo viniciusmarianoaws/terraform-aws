5814d02c8f7f6e636f02f373f0fe7711b1a354f0
vinicius.mariano <vinicius.mariano@darede.com.br>
2024-03-13 21:47:21 -0300

    terraform

---
+++ 02-configuration-language/04-outputs/main.tf
@@ -0,0 +1,26 @@
+# https://developer.hashicorp.com/terraform/language/values/outputs
+
+provider "aws" {
+  region  = "us-east-1"
+  profile = "tf_xxxxxxx"
+
+  default_tags {
+    tags = {
+      "Project"    = "AWS com Terraform"
+      "Module"     = "Configuration Language"
+      "Component"  = "Outputs"
+      "CreatedAt"  = "2023-10-24"
+      "ManagedBy"  = "Terraform"
+      "Owner"      = "Vinicius"
+      "Repository" = "github.com/viniciusmarianoaws/terraform-aws"
+    }
+  }
+}
+
+resource "random_pet" "bucket" {
+  length = 6
+}
+
+resource "aws_s3_bucket" "random" {
+  bucket = random_pet.bucket.id
+}
